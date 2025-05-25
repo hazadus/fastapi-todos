d738cf8e0e66fafd954a99729885276ec2f74c59
hazadus <hazadus7@gmail.com>
2025-05-25 09:18:32 +0300

    feat(frontend): инициализировать базовое приложение Vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <div id="app">
+    <Header />
+    <main class="p-4">
+      <router-view />
+    </main>
+  </div>
+</template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="p-4 bg-gray-800 text-white">
+    <nav>
+      <ul class="flex space-x-4">
+        <li>
+          <router-link to="/" class="hover:underline">Главная</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
